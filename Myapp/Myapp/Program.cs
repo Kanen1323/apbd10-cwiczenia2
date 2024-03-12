cd6591cff69dfaf57b162ee07aad92f80863f3a0
s27705 <safari.rre.345@gmail.com>
2024-03-12 16:45:27 +0100

    Modyfikacja 2

--- Myapp/Myapp/Program.cs
+++ Myapp/Myapp/Program.cs
@@ -1,7 +1,10 @@
 ﻿
-
+using System;
 Console.WriteLine("Pizza hut");
 
 int margaritta = 30;
 int Salami = 45;
 int Hawajska = 42;
+
+Console.WriteLine("Choise the  pizza you want");
+String choise = Console.ReadLine();
