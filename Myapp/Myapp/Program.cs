fcd021a3365308939fd28f103f1e3d857d378f43
s27705 <safari.rre.345@gmail.com>
2024-03-12 16:52:19 +0100

    Modyfikacja 3

--- Myapp/Myapp/Program.cs
+++ Myapp/Myapp/Program.cs
@@ -2,9 +2,33 @@
 using System;
 Console.WriteLine("Pizza hut");
 
-int margaritta = 30;
+int Margaritta = 30;
 int Salami = 45;
 int Hawajska = 42;
 
-Console.WriteLine("Choise the  pizza you want");
+Console.WriteLine("Chose the  pizza you want: " +
+                  "1-margarita " + 
+                  "2-Salami " +
+                  "3-Hawajska ");
 String choise = Console.ReadLine();
+
+if (int.TryParse(choise, out int number))
+{
+    switch (number)
+    {
+        case 1:
+            Console.WriteLine("You chose margaritta ");
+            break;
+        case 2:
+            Console.WriteLine("You chose salami ");
+            break;
+        case 3:
+            Console.WriteLine("You chose hawajska ");
+            break;
+        
+    }
+    
+    
+}
+
+
